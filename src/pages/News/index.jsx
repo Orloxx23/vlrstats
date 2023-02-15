d1697d876b11391a657559f37cdd41e414ac90ba
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-02-15 17:37:12 -0500

    title added

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import "./news.css";
 import NewsCard from "./newsCards";
 
@@ -34,25 +35,51 @@ export default function News() {
   }, [news]);
 
   return (
-    <div className="news-container">
-      <div className="main-news">
-        <NewsCard
-          banner={lastPatch?.banner_url}
-          title={lastPatch?.title}
-          url={lastPatch?.url}
-          loading={loading}
-        />
-      </div>
-      <div className="more-news">
-        <div className="more-news-row">
-            <NewsCard banner={otherNews?.[0]?.banner_url} title={otherNews?.[0]?.title} url={otherNews?.[0]?.external_link || otherNews?.[0]?.url} loading={loading} />
-            <NewsCard banner={otherNews?.[1]?.banner_url} title={otherNews?.[1]?.title} url={otherNews?.[1]?.external_link || otherNews?.[1]?.url} loading={loading} />
+    <>
+      <Helmet>
+        <title>News | MyStats</title>
+      </Helmet>
+
+      <div className="news-container">
+        <div className="main-news">
+          <NewsCard
+            banner={lastPatch?.banner_url}
+            title={lastPatch?.title}
+            url={lastPatch?.url}
+            loading={loading}
+          />
         </div>
-        <div className="more-news-row">
-            <NewsCard banner={otherNews?.[2]?.banner_url} title={otherNews?.[2]?.title} url={otherNews?.[2]?.external_link || otherNews?.[2]?.url} loading={loading} />
-            <NewsCard banner={otherNews?.[3]?.banner_url} title={otherNews?.[3]?.title} url={otherNews?.[3]?.external_link || otherNews?.[3]?.url} loading={loading} />
+        <div className="more-news">
+          <div className="more-news-row">
+            <NewsCard
+              banner={otherNews?.[0]?.banner_url}
+              title={otherNews?.[0]?.title}
+              url={otherNews?.[0]?.external_link || otherNews?.[0]?.url}
+              loading={loading}
+            />
+            <NewsCard
+              banner={otherNews?.[1]?.banner_url}
+              title={otherNews?.[1]?.title}
+              url={otherNews?.[1]?.external_link || otherNews?.[1]?.url}
+              loading={loading}
+            />
+          </div>
+          <div className="more-news-row">
+            <NewsCard
+              banner={otherNews?.[2]?.banner_url}
+              title={otherNews?.[2]?.title}
+              url={otherNews?.[2]?.external_link || otherNews?.[2]?.url}
+              loading={loading}
+            />
+            <NewsCard
+              banner={otherNews?.[3]?.banner_url}
+              title={otherNews?.[3]?.title}
+              url={otherNews?.[3]?.external_link || otherNews?.[3]?.url}
+              loading={loading}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
