72096ab32c037cab4f65255198102e77892e4621
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-02-22 19:05:03 -0500

    If there is user not show the form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Loader } from "../../components";
 import { AuthContext } from "../../context/AuthContext";
+import { Page404 } from "../";
 import "./login.css";
 
 export default function Login() {
@@ -17,49 +18,52 @@ export default function Login() {
     login(email, password);
   };
 
-  return (
-    <>
-      <Helmet>
-        <title>Sign In | MyStats</title>
-      </Helmet>
-      <div className="login">
-        <div className="login_left"></div>
-        <div className="login_right">
-          <div className="login_right_container">
-            <h1>Sign In</h1>
-            <form onSubmit={handlerSubmit} className="login_right_form">
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                disabled={loading}
-                required
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                disabled={loading}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <button type="submit" disabled={loading}>
-                {loading ? <Loader size={15} /> : "Continue"}
-              </button>
-            </form>
-            <div className="login_more_optiones">
-              <Link to="/">
-                <span>Forgot Password?</span>
-              </Link>
-              <Link to="/register">
-                {" "}
-                <span>Sign Up</span>
-              </Link>
+  if (localStorage.getItem("user")) {
+    return <Page404 />;
+  } else
+    return (
+      <>
+        <Helmet>
+          <title>Sign In | MyStats</title>
+        </Helmet>
+        <div className="login">
+          <div className="login_left"></div>
+          <div className="login_right">
+            <div className="login_right_container">
+              <h1>Sign In</h1>
+              <form onSubmit={handlerSubmit} className="login_right_form">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  disabled={loading}
+                  required
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  disabled={loading}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <button type="submit" disabled={loading}>
+                  {loading ? <Loader size={15} /> : "Continue"}
+                </button>
+              </form>
+              <div className="login_more_optiones">
+                <Link to="/">
+                  <span>Forgot Password?</span>
+                </Link>
+                <Link to="/register">
+                  {" "}
+                  <span>Sign Up</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -8,6 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { AuthContext } from "../../context/AuthContext";
 import { ThemeContext } from "../../context/ThemeContext";
 import { Helmet } from "react-helmet-async";
+import Page404 from "../Page404";
 
 export default function Register() {
   const [user, setUser] = useState("");
@@ -56,75 +57,78 @@ export default function Register() {
     createAccount(user, email, password);
   };
 
-  return (
-    <>
-      <Helmet>
-        <title>Sign Up | MyStats</title>
-      </Helmet>
-      <div className="login">
-        <div className="login_left"></div>
-        <div className="login_right">
-          <div className="login_right_container">
-            <h1>Sign Up</h1>
-            <form onSubmit={handlerSubmit} className="login_right_form">
-              <input
-                type="text"
-                placeholder="Username"
-                value={user}
-                onChange={(e) => setUser(e.target.value)}
-                disabled={loading}
-                required
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                disabled={loading}
-                required
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                disabled={loading}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <input
-                type="password"
-                placeholder="Password"
-                value={cPassword}
-                disabled={loading}
-                onChange={(e) => setcPassword(e.target.value)}
-                required
-              />
-              <button type="submit" disabled={loading}>
-                {loading ? <Loader size={15} /> : "Continue"}
-              </button>
-            </form>
-            <div className="login_more_optiones">
-              <div className="left_options">
-                <label className="checkBox">
-                  <input
-                    id="ch1"
-                    type="checkbox"
-                    checked={check}
-                    onChange={() => setCheck(!check)}
-                  />
-                  <div className="transition"></div>
-                </label>
-                <span>Accept terms and conditions</span>
-              </div>
+  if (localStorage.getItem("user")) {
+    return <Page404 />;
+  } else
+    return (
+      <>
+        <Helmet>
+          <title>Sign Up | MyStats</title>
+        </Helmet>
+        <div className="login">
+          <div className="login_left"></div>
+          <div className="login_right">
+            <div className="login_right_container">
+              <h1>Sign Up</h1>
+              <form onSubmit={handlerSubmit} className="login_right_form">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  value={user}
+                  onChange={(e) => setUser(e.target.value)}
+                  disabled={loading}
+                  required
+                />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  disabled={loading}
+                  required
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  disabled={loading}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={cPassword}
+                  disabled={loading}
+                  onChange={(e) => setcPassword(e.target.value)}
+                  required
+                />
+                <button type="submit" disabled={loading}>
+                  {loading ? <Loader size={15} /> : "Continue"}
+                </button>
+              </form>
+              <div className="login_more_optiones">
+                <div className="left_options">
+                  <label className="checkBox">
+                    <input
+                      id="ch1"
+                      type="checkbox"
+                      checked={check}
+                      onChange={() => setCheck(!check)}
+                    />
+                    <div className="transition"></div>
+                  </label>
+                  <span>Accept terms and conditions</span>
+                </div>
 
-              <Link to="/login">
-                {" "}
-                <span>Sign In</span>
-              </Link>
+                <Link to="/login">
+                  {" "}
+                  <span>Sign In</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
